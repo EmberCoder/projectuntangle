df5a9fa7246bff4e6c698435d7107503b1239f29
EmberCoder <anvi.rath@icloud.com>
2026-08-07 20:28:07 -0500

    added navigation bar and fixed layout for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import './App.css'
+import './App.css';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
 
   return (
-    <div className="stage">
-      <h1>Welcome back, Name!</h1>
+    <div>
 
-      <h2 className="stressQuestion">How stressed are you feeling today?</h2>   
+      <div className="HomepageContent">
+        <h1 className="Welcome">Welcome back, Name!</h1>
+        <h2 className="StressQuestion">How stressed are you feeling today?</h2> 
+
+        <NavBar/>
+
+      </div>
     </div>
       
-
-  )
+  );
 }
 
 export default App
