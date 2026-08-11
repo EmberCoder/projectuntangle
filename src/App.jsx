8d3f8cfee637541f514b7e371f6d8fa013b75f50
EmberCoder <anvi.rath@icloud.com>
2026-08-11 15:52:01 -0500

    Added radio buttons & submit button to StressQuestion and created the StressPopup for selected values over 3/5

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
+import { useState } from 'react';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
+import StressPopup from './components/StressQuestion/StressPopup';
+import StressQuestion from './components/StressQuestion/StressQuestion';
 
 function App() {
-
+  const [scale, setScale] = useState("");
+  const [showPopup, setShowPopup] = useState(false);
   return (
     <div>
 
       <div className="HomepageContent">
         <h1 className="Welcome">Welcome back, Name!</h1>
-        <h2 className="StressQuestion">How stressed are you feeling today?</h2> 
 
-        <NavBar/>
+        { showPopup ? (
+          <StressPopup scale={scale} />
+        ) : (
+          <StressQuestion setScale={setScale} setShowPopup={setShowPopup} scale={scale} />
+        )}
+
+        
+
+        <NavBar />
 
       </div>
+      
     </div>
       
   );
 }
 
-export default App
+export default App;
---
+++ src/components/StressQuestion/StressQuestion.jsx
@@ -0,0 +1,38 @@
+import './StressQuestion.css';
+
+const StressQuestion = ({setScale, setShowPopup, scale}) => {
+    const onOptionChange = (e) => {
+        setScale(e.target.value);
+    }
+    const handleSubmit = () => {
+        if (Number(scale) >= 3) {
+            setShowPopup(true);
+        }
+    }
+    return (
+        <div className="viewport">
+            <div className="QuestionText">
+                <h2>How stressed are you feeling today?</h2>
+
+                <div className="scale">
+                    <label htmlFor="scale-1">Not at all</label>
+                    <input type="radio" id="scale-1" name="scale" value="1" onChange={onOptionChange}/>
+
+                    <input type="radio" id="scale-2" name="scale" value="2" onChange={onOptionChange}/>
+                    <input type="radio" id="scale-3" name="scale" value="3" onChange={onOptionChange}/>
+                    <input type="radio" id="scale-4" name="scale" value="4" onChange={onOptionChange}/>
+
+                    <input type="radio" id="scale-5" name="scale" value="5" onChange={onOptionChange}/>
+                    <label htmlFor="scale-5">Very</label>
+
+                </div>
+
+            <div className="submit">
+                <button class="SubmitButton" onClick={handleSubmit}>Submit</button>
+            </div>
+            </div>
+        </div>
+    );
+};
+
+export default StressQuestion;
