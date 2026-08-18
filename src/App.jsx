506b027ecc08bac2f6645f3fcde3e5caa62f3552
EmberCoder <anvi.rath@icloud.com>
2026-08-18 15:25:36 -0500

    Minor bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import SelfCare from './selfcare';
