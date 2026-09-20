657cbd78fc75f941132588079c4388beed2a2a1d
EmberCoder <anvi.rath@icloud.com>
2026-09-20 13:35:44 -0500

    feat: update Firebase configuration to use environment variables and enhance .gitignore

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCnhjb-gwaPOUghPdWOpDbAeI8yXNVt7bg",
-  authDomain: "serenity-b4b72.firebaseapp.com",
-  projectId: "serenity-b4b72",
-  storageBucket: "serenity-b4b72.firebasestorage.app",
-  messagingSenderId: "749474732425",
-  appId: "1:749474732425:web:143a6e2fd1b20f2382a958",
-  measurementId: "G-WFLEV3WFNR"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
